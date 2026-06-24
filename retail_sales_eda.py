ff1502f6f4526c151e33eb7bd4ec2978d7b239d4
mathi <nilamoon5679@gmail.com>
2026-06-24 16:34:36 +0530

    Add files via upload

---
+++ retail_sales_eda.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Display settings
+pd.set_option('display.max_columns', None)
+
+# Visualization style
+sns.set_style("whitegrid")
+plt.rcParams['figure.figsize'] = (10,6)
